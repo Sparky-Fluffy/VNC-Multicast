3433b6c5e61df46d9346d9667ac9d06af9b8e17a
Sparky-Fluffy <andrewdev276@gmail.com>
2025-03-15 18:01:39 +0700

    Первый рефакторинг черновика.

--- proxy/Program.cs
+++ proxy/Program.cs
@@ -5,66 +5,61 @@ namespace proxy;
 
 class Retranslator
 {
-    public int port { get; private set; }
-    public IPAddress ip { get; private set; }
-    public Socket socket { get; private set; }
+    public int _port { get; private set; }
+    public IPAddress _ip { get; private set; }
+    public Socket _socket { get; private set; }
 
-    public Retranslator()
+    public Retranslator(byte[] addr, int port)
     {
-        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+        _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
                 ProtocolType.Tcp);
-        ip = new IPAddress(new byte[] { 0, 0, 0, 0 } );
+        _ip = new IPAddress(addr);
+        _port = port;
     }
 
-    private void LogMessage(ConsoleColor color, string msg)
+    private void makeHandshakes()
     {
-        Console.ForegroundColor = color;
-        Console.WriteLine(msg);
-        Console.ForegroundColor = ConsoleColor.White;
+        try
+        {
+            _socket.Connect(_ip, _port);
+
+            byte[] protocolVersion = new byte[12];
+            _socket.Receive(protocolVersion, protocolVersion.Length, 0);
+
+            _socket.Send(protocolVersion, protocolVersion.Length, 0);
+
+            byte[] securityTypes = new byte[2];
+            _socket.Receive(securityTypes, securityTypes.Length, 0);
+
+            byte[] securityType = new byte[] { securityTypes[0] };
+            _socket.Send(securityType, securityType.Length, 0);
+
+            byte[] securityHandshake = new byte[1];
+            _socket.Receive(securityHandshake, securityHandshake.Length, 0);
+        } catch (SocketException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Environment.Exit(1);
+        }
     }
 
     private void ClientInit()
     {
         byte[] sharedFlag = new byte[1] { 0 };
-        socket.Send(sharedFlag, sharedFlag.Length, 0);
-    }
-
-    public void Connect(byte[] addr, int port)
-    {
-        ip = new IPAddress(addr);
         try
         {
-            socket.Connect(ip, port);
-
-            byte[] protocolVersion = new byte[12];
-            socket.Receive(protocolVersion, protocolVersion.Length, 0);
-
-            socket.Send(protocolVersion, protocolVersion.Length, 0);
-
-            byte[] securityTypes = new byte[2];
-            socket.Receive(securityTypes, securityTypes.Length, 0);
-
-            byte[] securityType = new byte[] { securityTypes[0] };
-            socket.Send(securityType, securityType.Length, 0);
-
-            byte[] securityHandshake = new byte[1];
-            socket.Receive(securityHandshake, securityHandshake.Length, 0);
-
-            switch (securityHandshake[0])
-            {
-                case 0:
-                    LogMessage(ConsoleColor.Green, "Connected...");
-                    break;
-                case 1:
-                    LogMessage(ConsoleColor.Red, "Not connected...");
-                    break;
-            }
-            Console.ForegroundColor = ConsoleColor.White;
-            ClientInit();
+            _socket.Send(sharedFlag, sharedFlag.Length, 0);
         } catch (SocketException ex)
         {
-            LogMessage(ConsoleColor.Red, ex.Message);
+            Console.WriteLine(ex.Message);
+            Environment.Exit(1);
         }
+    }
+
+    public void Connect()
+    {
+        makeHandshakes();
+        ClientInit();
     }
 }
 
@@ -72,7 +67,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Retranslator retranslator = new Retranslator();
-        retranslator.Connect(new byte[] { 127, 0, 0, 1 }, 5900);
+        Retranslator client = new Retranslator(new byte[] { 127, 0, 0, 1 },
+                5900);
+        client.Connect();
     }
 }
