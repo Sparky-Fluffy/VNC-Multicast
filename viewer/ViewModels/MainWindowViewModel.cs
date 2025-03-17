a48a2ac0e9a55d31e48ff06ae73ecf68e9b637d8
TheTojer <nikitapirogov26@gmail.com>
2025-03-17 19:17:49 +0700

    Created new directory Styling containing app styles. Made changes to MainWindow constructor to pass the MainWindowViewModel in. Created fields and properties in MainWindowViewModel to change screen percentage. Redacted App.axaml.cs to match the changes. Made test window view and its logic.

--- viewer/ViewModels/MainWindowViewModel.cs
+++ viewer/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace viewer.ViewModels;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Platform;
+
+namespace viewer.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private double percentWidth = 0.5;
+    private double percentHeight = 0.5;
+
+    public double PercentWidth { get => percentWidth; set => percentWidth = value; }
+    public double PercentHeight { get => percentHeight; set => percentHeight = value; }
 }
--- viewer/Views/MainWindow.axaml.cs
+++ viewer/Views/MainWindow.axaml.cs
@@ -1,11 +1,44 @@
+using System;
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform;
+using viewer.ViewModels;
 
 namespace viewer.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    private int screenWidth = 1000;
+    private int screenHeight = 1000;
+    private MainWindowViewModel viewModel;
+
+    public MainWindow(MainWindowViewModel model)
     {
         InitializeComponent();
+        
+        viewModel = model;
+
+        screenWidth = Screens.Primary!.Bounds.Width;
+        screenHeight = Screens.Primary!.Bounds.Height;
+
+        Width = screenWidth * viewModel.PercentWidth;
+        Height = screenHeight * viewModel.PercentHeight;
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+        }
+
+        Console.WriteLine($"Click! Celsius={celsius.Text}");
     }
 }
