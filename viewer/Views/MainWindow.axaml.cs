09da2c8e9f56be41bf3971db672ccaee8ed8e690
TheTojer <nikitapirogov26@gmail.com>
2025-03-19 01:24:07 +0700

    Added custom title bar like in MacOS. Added window round corners :)

--- viewer/Views/MainWindow.axaml.cs
+++ viewer/Views/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Diagnostics;
+using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Platform;
 using viewer.ViewModels;
@@ -11,6 +14,9 @@ public partial class MainWindow : Window
 {
     private int screenWidth = 1000;
     private int screenHeight = 1000;
+
+    private bool isWindowDragInEffect = false;
+    private Point cursorPositionAtWindowDragStart = new(0, 0);
 
     public MainWindow()
     {
@@ -29,7 +35,32 @@ public partial class MainWindow : Window
             Width = screenWidth * vm.PercentWidth;
             Height = screenHeight * vm.PercentHeight;
         }
+
+        SessionPicker.ItemsSource = new string[]
+                                    {"cat", "camel", "cow", "chameleon", "mouse", "lion", "zebra" }.OrderBy(x => x);
     }
+
+    private void WindowDragHandle_OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        if (isWindowDragInEffect)
+        {
+            Point currentCursorPosition = e.GetPosition(this);
+            Point cursorPositionDelta = currentCursorPosition - cursorPositionAtWindowDragStart;
+
+            Position = this.PointToScreen(cursorPositionDelta);
+        }
+    }
+
+    private void WindowDragHandle_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if ((e.Source is Control sourceControl) && (sourceControl.Name == "WindowDragHandle"))
+        {
+            isWindowDragInEffect = true;
+            cursorPositionAtWindowDragStart = e.GetPosition(this);
+        }
+    }
+
+    private void WindowDragHandle_OnPointerReleased(object? sender, PointerReleasedEventArgs e) => isWindowDragInEffect = false;
 
     private void Button_OnClick(object? sender, RoutedEventArgs e)
     {
