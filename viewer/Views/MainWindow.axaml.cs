68fecb328f084cc54ca79b0ce519dc6344a3b8bb
TheTojer <nikitapirogov26@gmail.com>
2025-03-20 01:44:43 +0700

    I'm tired of writing and want to sleep. I made demo for second page with screen view and placed there example of image and three buttons. First will have audio-switch functionality, second is call cancel button and last is expand window button

--- viewer/Views/MainWindow.axaml.cs
+++ viewer/Views/MainWindow.axaml.cs
@@ -15,8 +15,9 @@ public partial class MainWindow : Window
     private int screenWidth = 1000;
     private int screenHeight = 1000;
 
-    private bool isWindowDragInEffect = false;
-    private Point cursorPositionAtWindowDragStart = new(0, 0);
+    private string sessionName = "";
+
+    private bool isFullScreen = false;
 
     public MainWindow()
     {
@@ -40,41 +41,42 @@ public partial class MainWindow : Window
                                     {"cat", "camel", "cow", "chameleon", "mouse", "lion", "zebra" }.OrderBy(x => x);
     }
 
-    private void WindowDragHandle_OnPointerMoved(object? sender, PointerEventArgs e)
+    private void Select_OnClick(object? sender, RoutedEventArgs e)
     {
-        if (isWindowDragInEffect)
-        {
-            Point currentCursorPosition = e.GetPosition(this);
-            Point cursorPositionDelta = currentCursorPosition - cursorPositionAtWindowDragStart;
-
-            Position = this.PointToScreen(cursorPositionDelta);
-        }
+        Page1.IsVisible = false;
+        Page2.IsVisible = true;
     }
 
-    private void WindowDragHandle_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    private void Cancel_OnClick(object? sender, RoutedEventArgs e)
     {
-        if ((e.Source is Control sourceControl) && (sourceControl.Name == "WindowDragHandle"))
-        {
-            isWindowDragInEffect = true;
-            cursorPositionAtWindowDragStart = e.GetPosition(this);
-        }
+        Page1.IsVisible = true;
+        Page2.IsVisible = false;
+        SetWindowNormal();
     }
 
-    private void WindowDragHandle_OnPointerReleased(object? sender, PointerReleasedEventArgs e) => isWindowDragInEffect = false;
-
-    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    private void Expand_OnClick(object? sender, RoutedEventArgs e)
     {
-        // if (double.TryParse(celsius.Text, out double C))
-        // {
-        //     var F = C * (9d / 5d) + 32;
-        //     fahrenheit.Text = F.ToString("0.0");
-        // }
-        // else
-        // {
-        //     celsius.Text = "0";
-        //     fahrenheit.Text = "0";
-        // }
+        if (isFullScreen) SetWindowNormal();
+        else SetWindowMaximized();
+    }
 
-        // Console.WriteLine($"Click! Celsius={celsius.Text}");
+    private void SetWindowNormal()
+    {
+        WindowState = WindowState.Normal;
+        SystemDecorations = SystemDecorations.Full;
+        isFullScreen = false;
+    }
+
+    private void SetWindowMaximized()
+    {
+        WindowState = WindowState.FullScreen;
+        SystemDecorations = SystemDecorations.None;
+        ViewHeader.IsVisible = false;
+        ScreenView.Width = screenWidth;
+        ScreenView.Height = screenHeight;
+        ScreenView.Margin = new Thickness(0, 0, 0, 0);
+        FunctionButtons.Margin = new Thickness(0, 0, 0, 0);
+        isFullScreen = true;
+        
     }
 }
