22925d8c83ce8ea25f6bc0274804dea50fbdf3d3
Sparky-Fluffy <andrewdev276@gmail.com>
2025-03-26 23:04:35 +0700

    Заготовка для функционала добавления конфигурации.

--- proxy/Program.cs
+++ proxy/Program.cs
@@ -1,11 +1,29 @@
-﻿namespace proxy;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace proxy;
 
 class Program
 {
+    static async void RunAppAsync(string[] args)
+    {
+        using IHost host = Host.CreateApplicationBuilder(args).Build();
+        IConfiguration config =
+            host.Services.GetRequiredService<IConfiguration>();
+
+        string? ip = config.GetValue<string>("IP");
+        int port = config.GetValue<int>("port");
+
+        Retranslator client = new Retranslator(new byte[] { 127, 0, 0, 1 }, 5900
+                , Encodings.Raw);
+        client.Connect();
+
+        await host.RunAsync();
+    }
+
     static void Main(string[] args)
     {
-        Retranslator client = new Retranslator(new byte[] { 127, 0, 0, 1 },
-                5900, Encodings.Raw);
-        client.Connect();
+        RunAppAsync(args);
     }
 }
