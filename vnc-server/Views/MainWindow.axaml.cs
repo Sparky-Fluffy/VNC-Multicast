3208dae0a52ec36b57fe7c3133080d976c4e7dda
Sparky-Fluffy <andrewdev276@gmail.com>
2025-04-08 21:49:17 +0700

    Getting data with valid.

--- vnc-server/Views/MainWindow.axaml.cs
+++ vnc-server/Views/MainWindow.axaml.cs
@@ -3,14 +3,40 @@ using Avalonia.Interactivity;
 using System;
 using MsBox.Avalonia;
 using MsBox.Avalonia.Enums;
+using MsBox.Avalonia.Base;
 
 namespace vnc_server.Views;
 
 public partial class MainWindow : Window
 {
+    public bool? hideWindow;
+    public int port;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void SetParamsAsync()
+    {
+        IMsBox<ButtonResult> box = null;
+        hideWindow = isHideWin.IsChecked;
+
+        if (tbPortData.Text == null)
+            box = MessageBoxManager.GetMessageBoxStandard("Пустое значение",
+                    "Не введён порт для запуска", ButtonEnum.Ok);
+        else
+        {
+            int.TryParse(tbPortData.Text, out port);
+            if (port < 5900 || port > 5906)
+                box = MessageBoxManager.GetMessageBoxStandard("Неверный порт",
+                    "Порт должен быть от 5900 до 5906", ButtonEnum.Ok);
+        }
+        if (box != null)
+        {
+            await box.ShowWindowDialogAsync(this);
+            Environment.Exit(1);
+        }
     }
 
     private void RunServer()
@@ -18,9 +44,9 @@ public partial class MainWindow : Window
 #if DEBUG
         Console.WriteLine($"Close window: {isHideWin.IsChecked}");
         Console.WriteLine($"Port: {tbPortData.Text}");
-        Console.WriteLine($"tbPortData.Text == string.Empty ({tbPortData.Text ==
-            string.Empty})");
+        Console.WriteLine($"PortData.Text == null ({tbPortData.Text == null})");
 #endif
+        SetParamsAsync();
     }
 
     private void StartServer(object sender, RoutedEventArgs e)
