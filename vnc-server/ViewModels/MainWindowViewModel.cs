e9c6271471890b1098a76206a66a48a3862e4d59
Sparky-Fluffy <andrewdev276@gmail.com>
2025-04-08 09:55:03 +0700

    Edit gitignore and add new project for vnc server.

---
+++ vnc-server/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace vnc_server.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ vnc-server/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace vnc_server.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
