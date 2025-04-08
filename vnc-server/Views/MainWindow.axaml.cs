b3b12f33c49d5b83698c05b7a05dc30a4633c9da
Sparky-Fluffy <andrewdev276@gmail.com>
2025-04-08 20:15:47 +0700

    Test message box.

--- vnc-server/Views/MainWindow.axaml.cs
+++ vnc-server/Views/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 
 namespace vnc_server.Views;
 
@@ -17,6 +19,11 @@ public partial class MainWindow : Window
         Console.WriteLine(isHideWin.IsChecked);
         Console.WriteLine(tbPortData.Text);
 #endif
+        var box = MessageBoxManager
+            .GetMessageBoxStandard("Caption", "Are you sure you would like to" +
+                    "delete appender_replace_page_1?",
+                ButtonEnum.YesNo);
+        box.ShowAsync();
     }
 
     private void StartServer(object sender, RoutedEventArgs e)
