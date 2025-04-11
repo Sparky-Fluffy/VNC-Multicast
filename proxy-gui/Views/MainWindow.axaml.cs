a143012fcf780e8528ab3314e25212c7f8315b0f
Sparky-Fluffy <andrewdev276@gmail.com>
2025-04-11 11:20:17 +0700

    Add first prototype event handler for button.

--- proxy-gui/Views/MainWindow.axaml.cs
+++ proxy-gui/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
 
 namespace proxy_gui.Views;
 
@@ -7,5 +9,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void StartProxyServer(object? sender, RoutedEventArgs e)
+    {
+#if DEBUG
+        Console.WriteLine($"Server Address = {ServerAddressTb.Text}");
+        Console.WriteLine("Group multicast address = " +
+                $"{GroupMulticastAddressTb.Text}");
+        Console.WriteLine($"Hide window = {isHideWinCheckBox.IsChecked}");
+#endif
     }
 }
