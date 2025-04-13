100965843d4981f01a1fc8f85e1ebf6c1a6d5f71
Sparky-Fluffy <andrewdev276@gmail.com>
2025-04-13 23:01:51 +0700

    I hate GUI with all my heart.

--- proxy-gui/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace proxy_gui.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- proxy-gui/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace proxy_gui.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
